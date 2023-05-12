d6496bc212ef279cf15c2d5c74755cd71d1a099f
Marinabauer10 <Marina.Bauer@Vodafone.com>
2023-05-12 14:57:35 +0200

    insert new comment into bikeshare.py #Create def for Filters

--- bikeshare.py
+++ bikeshare.py
@@ -11,6 +11,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+#Create def for Filters
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
